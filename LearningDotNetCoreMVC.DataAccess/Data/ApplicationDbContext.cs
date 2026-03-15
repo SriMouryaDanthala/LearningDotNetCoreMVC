cc3a4590b7170bac5ddb05a065b642239f76b542
SriMouryaDanthala <danthalasrimourya@gmail.com>
2026-03-15 23:11:17 +0530

    Initial CRUD Version for categories, 1st commit

---
+++ LearningDotNetCoreMVC.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using LearningDotNetCoreMVC.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningDotNetCoreMVC.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "SelfHelp", DisplayOrder = 3 }
+            );
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        
+    }
+}
---
+++ LearningDotNetCoreMVC.Models/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningDotNetCoreMVC.Models.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+    }
+}
